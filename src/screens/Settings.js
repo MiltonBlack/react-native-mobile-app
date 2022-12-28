b715e445ed33fd33e491b428941262c62347a49d
MiltonBlack <arziblack2@gmail.com>
2022-12-28 11:50:59 +0100

    dark Mode style created but not integrated

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
 const Settings = () => {
+  const Seperator = () => {
+    return <View style={{ height: 1, width: '100%', backgroundColor: '#cce' }} />
+  }
   return (
-    <View>
-      <Text style={{color:'black'}}>Settings</Text>
+    <View style={{ margin: 20 }}>
+      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>My Profile</Text>
+      <Seperator />
+      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>Appearance Settings</Text>
+      <Seperator />
+      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>Rating</Text>
+      <Seperator />
+      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>Review</Text>
+      <View style={{ height: 350 }}></View>
+      <Button
+        title='Light/Dark Mode'
+        color="#841584"
+      />
     </View>
   )
 }
---
+++ src/styles/darkMode.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+const darkMode = StyleSheet.create({
+    listHeader: {
+        height: 55,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headStyle: {
+        color: '#333',
+        fontSize: 27,
+        fontWeight: 'bold'
+    },
+    items: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5,
+        margin: 10,
+        backgroundColor: 'black',
+        shadowColor: 'white',
+        borderRadius: 23,
+        shadowOffset: {
+            width: 15,
+            height: 20
+        },
+        shadowOpacity: .3,
+        shadowRadius: 10
+    },
+    title: {
+        fontWeight: '600',
+        fontSize: 20,
+        marginHorizontal: 10,
+        padding: 10,
+        color: 'white',
+        textAlign: 'center',
+
+    }
+})
+
+export default darkMode;
