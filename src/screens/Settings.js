b56e97588edc8b367488281363f80ef7b524f2f5
MiltonBlack <arziblack2@gmail.com>
2023-03-12 11:07:29 +0100

    Added a youtube subscribe link

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,7 +1,20 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Linking, Alert } from 'react-native'
+import React, { useCallback } from 'react'
 
 const Settings = () => {
+  const url = 'https://www.youtube.com/@engineeringgraphics4978';
+
+  const canOpenUrlLinkButton = ({ url, children }) => {
+    const handlePress = useCallback(async () => {
+      const supported = await Linking.canOpenURL(url);
+      if (supported) {
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`Invalid URL Link: ${url}`)
+      }
+    }, [url])
+    return <Button title={children} onPress={handlePress} />
+  };
   const Seperator = () => {
     return <View style={{ height: 1, width: '100%', backgroundColor: '#cce' }} />
   }
@@ -13,9 +26,10 @@ const Settings = () => {
       <Seperator />
       <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>Update</Text>
       <Seperator />
-      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>Subscribe to our Channel</Text>
+      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }} onPress={()=> Linking.openURL(url)}>Subscribe to our Tutorial Channel for more content</Text>
+      <canOpenUrlLinkButton url={url}>Subscribe to our channel</canOpenUrlLinkButton>
       <View style={{ height: 350 }}></View>
-      <Button title='hello'/>
+      {/* <Button title='hello' /> */}
     </View>
   )
 }
